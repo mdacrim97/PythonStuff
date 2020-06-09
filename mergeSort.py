2a3bc83962f46c718f977696326c695189e4cada
mdacrim97 <mdacrim97@gmail.com>
2020-06-09 10:10:41 -0400

    merge sort and 301 generator

--- mergeSort.py
+++ mergeSort.py
@@ -1,4 +1,3 @@
-import statistics as s
 
 def mergeSort(array):
 	if(len(array) >1):
@@ -31,11 +30,3 @@ def mergeSort(array):
 			j += 1
 			k += 1
 	
-l = [0.733,   	0.862,   	0.868,   	0.910,   	0.921,   	0.931,   	0.980,   	1.009,   
-1.039,   	1.048,   	1.092 ,  	1.119  , 	1.130,   	1.156,   	1.231,   	1.039] #-0.43
-mergeSort(l)
-#print("post sort %s" %l)
-print("median " + str(s.median(l)))
-#print("mean " + str(s.mean(l)))
-#print("std deviation " + str(s.pstdev(l)))
-#print("variance " + str(s.variance(l)))
